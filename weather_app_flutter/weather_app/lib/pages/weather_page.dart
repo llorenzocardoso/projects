6cd18ed05245c706a88bc61831066fae34f0ebee
Lorenzo Cardoso <102180476+llorenzocardoso@users.noreply.github.com>
2024-09-20 20:30:16 -0300

    update 2024-09-20

--- weather_app_flutter/weather_app/lib/pages/weather_page.dart
+++ weather_app_flutter/weather_app/lib/pages/weather_page.dart
@@ -11,7 +11,7 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
-  final _weatherService = WeatherService('6b5f5034bfeb059a6bb54e09bca5ada6');
+  final _weatherService = WeatherService('YOUR_API_KEY');
   Weather? _weather;
   bool _isNight = false;
   bool _isDarkMode = false;
